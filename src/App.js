0e8d8274b85329181eec02e3d8fb27a27c30f609
mạnh nguyễn bá <jizero2003@gmail.com>
2024-07-15 07:42:38 +0700

    update 15/7

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { ColorModeContext, useMode } from "./theme";
+import { useState } from 'react';
+import { Navigate, useNavigate } from "react-router-dom";
 import { Route, Routes } from 'react-router-dom'
 import { CssBaseline, ThemeProvider } from "@mui/material"
 import Topbar from "./scenes/global/Topbar"
@@ -10,30 +12,49 @@ import Feedback from "./scenes/feedback/Feedback"
 import Conversation from "./scenes/converation/Conversation"
 import Chart from "./scenes/Chart/Chart";
 import CreateAcctount from "./scenes/create/CreateAccount";
+import LoginPage from "./login/LoginPage";
 // import Assistants from "./scenes/assistants/Assistans";
 
 function App() {
   const [theme, colorMode] = useMode();
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // Trạng thái đăng nhập
+  const navigate = useNavigate();
+
+  // Hàm để xử lý đăng nhập
+  const handleLogin = (credentials) => {
+    setIsLoggedIn(true);
+    navigate('/');
+  };
+
 
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-            <Sidebar />
-          <main className="content">
-            <Topbar />
+        {isLoggedIn ? (
+            <>
+              <Sidebar />
+              <main className="content">
+                <Topbar />
+                <Routes>
+                  <Route path="/" element={<Dashboard />} />
+                  <Route path="/users-managment" element={<Users />} />
+                  <Route path="/notebooks-managment" element={<Notebooks />} />
+                  <Route path="/create-account" element={<CreateAcctount />} />
+                  <Route path="/feedback" element={<Feedback />} />
+                  {/* <Route path="/assistants" element={<Assistants />} /> */}
+                  <Route path="/conversation" element={<Conversation />} />
+                  <Route path="/column-chart" element={<Chart />} />
+                </Routes>
+              </main>
+            </>
+          ) : (
             <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/users-managment" element={<Users />} />
-              <Route path="/notebooks-managment" element={<Notebooks />} />
-              <Route path="/create-account" element={<CreateAcctount />} />
-              <Route path="/feedback" element={<Feedback />} />
-              {/* <Route path="/assistants" element={<Assistants />} /> */}
-              <Route path="/conversation" element={<Conversation />} />
-              <Route path="/column-chart" element={<Chart />} />
+              <Route path="/login" element={<LoginPage onLogin={handleLogin} />} />
+              <Route path="*" element={<Navigate to="/login" replace />} />
             </Routes>
-          </main>
+          )}
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
---
+++ src/login/LoginPage.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Checkbox, Box, Grid,Paper, Avatar, TextField, Button, Typography,Link, FormControlLabel  } from '@mui/material'
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+
+const LoginPage = () => {
+
+
+    return(
+       <Box m='0' p='0'
+            width="100%" height='100%'
+            display='flex'
+            justifyContent='center'
+            alignItems='center'
+       >
+            <Box
+                m='20px'
+                width='400px'
+                backgroundColor='red'
+                height='600px'
+            >
+
+            </Box>
+
+       </Box>
+    )
+}
+
+export default LoginPage
